ce32d55658f22fbe21400d5e88a910a35eb89414
aliabbasrizvi20 <rockcodes0@gmail.com>
2026-04-13 12:44:35 +0530

    chagned the live  URLs

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ const carRouter = require("./app/routes/car.router");
 const userRouter = require("./app/routes/router");
 const orderRouter = require("./app/routes/order.router");
 const app = express();
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "http://localhost:3001",
-  credentials: true 
-}));
-app.options("*", cors({
-  origin: "http://localhost:3001",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3001",
+      "https://drivos.netlify.app",
+    ],
+    credentials: true,
+  }),
+);
+app.options(
+  "*",
+  cors({
+   origin: ["http://localhost:3001",
+      "https://drivos.netlify.app",
+    ],
+    credentials: true,
+  }),
+);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Mongoose Connected"))
@@ -34,53 +43,3 @@ app.use("/orders", orderRouter);
 app.listen(PORT, () => {
   console.log(`Server is live on port: ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const handle = require('./app/server')
-// const express = require('express')
-// const path = require('path');
-// const { PORT } = require('./app/common/config');
-// const httpServer = require("http").createServer(handle)
-// const port = PORT || 3000
-// handle.use(express.static(path.join(__dirname, "/build")));
-
-// handle.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/build', 'index.html'));
-// });
-
-// httpServer.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
